b64c1b67e6ba50ee6b84d06dd0edbea2ef39a19e
hemali_patel <43220738+hsp48@users.noreply.github.com>
2020-12-16 18:34:30 -0500

    Update code2.py

--- code2.py
+++ code2.py
@@ -70,7 +70,7 @@ server.append(PatientOxygen)
 PatientNotes = input("Enter any Notes: ")
 server.append(PatientNotes)
 
-createservertext = open("servernotes.txt", "a+")
+createservertext = open("servernotes.txt", "w+")
 for line in server:
     w = ('\t'.join(line))
     createservertext.write(w)
